2a0ae034ad345cbac1e7049d3cbb0a8f10b704d6
cheneym <cheneymao94@gmail.com>
2016-09-26 15:22:43 -0700

    Populate initial chat boxes

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,21 @@
-// YOUR CODE HERE:
+var apiURL = 'https://api.parse.com/1/classes/messages';
+
+var app = {};
+
+app.init = function() {
+
+};
+
+$.get(apiURL, function(data) {
+  var messages = data.results;
+  console.log(data);
+  for (var i = 0; i < messages.length; i++) {
+    var $chats = $('#chats');
+    var $chatBox = $('<div></div>').addClass('chat');
+    var $name = $('<p></p>').addClass('username').text(messages[i].username);
+    var $text = $('<p></p>').text(messages[i].text);
+    $chatBox.append($name);
+    $chatBox.append($text);
+    $chats.append($chatBox);
+  }
+});
