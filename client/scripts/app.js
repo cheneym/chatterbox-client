8f6606ec25bc1c6f81f964226c99a59462135fb7
cheneym <cheneymao94@gmail.com>
2016-09-26 15:53:05 -0700

    Add text input area and submit button

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,10 +9,15 @@ app.init = function() {
 $.get(apiURL, function(data) {
   var messages = data.results;
   console.log(data);
+  var $selector = $('.chatroomNames');
+  
+
   for (var i = 0; i < messages.length; i++) {
     var $chats = $('#chats');
+    //var roomnames = 
+    // console.log(messages[i].roomname)
     var $chatBox = $('<div></div>').addClass('chat');
-    var $name = $('<p></p>').addClass('username').text(messages[i].username);
+    var $name = $('<p></p>').addClass('username').text(messages[i].username + ':');
     var $text = $('<p></p>').text(messages[i].text);
     $chatBox.append($name);
     $chatBox.append($text);
